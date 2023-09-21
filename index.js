8ff7137daa5a29749bef5c820b35643cf90fba7c
AnnaLevitski <annalevitskiqa@gmail.com>
2023-09-21 13:40:04 +0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const fs = require('fs');
 const imgs = "./imgs";
 let i = 0;
 let totalHeight = 1240;
-
+// const buildTemplate = (tupe) => {
+    //     return `
+    //         <div style = "font-size: 10px; color: black">
+    //             <span>${tupe === 'header'?'HEADER': 'FOOTER'} </span>
+    //             <span class="date"> </span>
+    //             <span class="title"> </span>
+    //             <span class="url"> </span>
+    //             <span class="pageNumber"> </span>
+    //    
+            //  <span class="totalPages"> </span>
+    //         </div>
+    //     `
+    // }
 const URL = 'https://school.kotar.cet.ac.il/KotarApp/Viewer.aspx?nBookID=103947541#1.0.6.default';
 //const URL = 'https://www.npmjs.com/package/puppeteer';
 
@@ -53,6 +65,27 @@ const URL = 'https://school.kotar.cet.ac.il/KotarApp/Viewer.aspx?nBookID=1039475
         path: `imgs/${i++}.png`, 
     });
     await tor(page, pageHeight);
+
+
+// await page.pdf({
+//     path: 'doc.pdf',
+//     timeout: 30000,
+//     displayHeaderFooter: true,
+//    // headerTemplate: buildTemplate('header'),
+//    // footerTemplate: buildTemplate('footer'),
+//     printBackground: true,
+//     landscape: false,
+//     format: 'A4',
+//     margin: {
+//         top: 100,
+//         right: 40,
+//         bottom: 100,
+//         left: 40,
+//     },
+    
+// })
+
+
     await browser.close();
 })();
 
@@ -78,8 +111,21 @@ async function takeScreenshot(page) {
     });
      
 }
-
-
+//?
+async function convertPNGtoPDF(imagePaths, outputPath) {
+    const page2 = await browser.newPage();
+    await page2.goto('about:blank');
+    
+    for (const imagePath of imagePaths) {
+        await page2.goto(`file://${imagePath}`, { waitUntil: 'networkidle0' });
+        
+    }
+    await page2.pdf({ 
+        path: outputPath, 
+        format: 'A4', 
+        printBackground: true 
+    });
+}
 
 
 
